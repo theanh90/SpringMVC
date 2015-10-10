b03c113dd88b22a5326f601e963eaf1d587f8b31
theanh90 <AnhBui@Anhs-MacBook-Pro.local>
2015-10-10 10:42:30 +0700

    modify constructor

--- src/spittr/data/SpittleRepositoryInstance.java
+++ src/spittr/data/SpittleRepositoryInstance.java
@@ -14,9 +14,9 @@ public class SpittleRepositoryInstance implements SpittleRepository{
 	@Override
 	public List<Spittle> findSpittles(long max, int count) {
 		List<Spittle> result = new ArrayList<>();
-		Spittle s01 = new Spittle("mess01", new Date(), 5.5, 6.6);
-		Spittle s02 = new Spittle("mess02", new Date(), 7.7, 7.7);
-		Spittle s03 = new Spittle("mess03", new Date(), 8.8, 8.8);
+		Spittle s01 = new Spittle(1l, "mess01", new Date(), 5.5, 6.6);
+		Spittle s02 = new Spittle(2l, "mess02", new Date(), 7.7, 7.7);
+		Spittle s03 = new Spittle(3l, "mess03", new Date(), 8.8, 8.8);
 		result.add(s01);
 		result.add(s02);
 		result.add(s03);
